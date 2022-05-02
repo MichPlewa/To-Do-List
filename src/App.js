2124ef2d168b9b545ecf88cea1bf25e1b19c1bba
Michal Plewa <themipsial@gmail.com>
2022-05-02 11:27:57 +0200

    Props_Children

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Hero from './components/Hero/Hero.js';
 import SerchForm from './components/SearchForm/SearchForm.js';
 import List from './components/List/List.js';
+import Container from './components/Container/Container.js';
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <Hero />
       <SerchForm />
       <List />
-    </div>
+    </Container>
   );
 };
 
---
+++ src/components/Container/Container.js
@@ -0,0 +1,11 @@
+import styles from './container.module.scss';
+
+const Container = props => {
+    return (
+        <div className={styles.container}>
+            {props.children}
+        </div>
+    );
+}
+
+export default Container;
