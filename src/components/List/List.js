c3ff7ba652ef789b04103db1f855c367ea5b4fdd
Michal Plewa <themipsial@gmail.com>
2022-05-02 13:59:50 +0200

    add_column_start

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,20 +1,32 @@
 import styles from './list.module.scss';
-import Column from '../Column/Column.js';
+import Column from './../Column/Column';
+import ColumnForm from './../ColumnForm/ColumnForm';
+import { useState } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
-  return (
-  <div className={styles.list}>
-    <header className={styles.header}>
-      <h1 className={styles.title}>THINGS TO DO <span>SOON</span></h1>
-    </header>
-    <p className={styles.description}>Intresting things I want to check out!</p>
-    <section className={styles.columns}>
-      <Column title='Books' icon='book'/>
-      <Column title='Movies' icon='film'/>
-      <Column title='Games' icon='gamepad'/>
-    </section>
-  </div>
-  )
-}
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
 
-export default List
+	const addColumn = newColumn => {
+		setColumns([...columns, { id: shortid(), title: newColumn.title }]);
+	};
+
+	return (
+		<div className={styles.list}>
+			<header className={styles.header}>
+				<h2 className={styles.title}>Things to do<span>soon!</span></h2>
+			</header>
+			<p className={styles.description}>Interesting things I want to check out</p>
+			<section className={styles.columns}>
+				{columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
+			</section>
+			<ColumnForm action={addColumn} />
+		</div>
+	);
+};
+
+export default List;
